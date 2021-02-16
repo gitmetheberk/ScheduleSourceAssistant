ec5da1d47b2dccbac43320b3d9ead9968f7feda1
Matthew Becker <becker@tamu.edu>
2021-02-16 14:06:32 -0600

    Function scope ss_refresh.js

--- src/scripts/ss_refresh.js
+++ src/scripts/ss_refresh.js
@@ -1,10 +1,16 @@
 // [12] Code from source 12 modified for use below
-// Create the event object
-let eventObject = document.createEvent('Events');
-eventObject.initEvent('click', true, false);
 
-// Grab the refresh button by class (only one element on the page uses this class)
-let refresh = document.getElementsByClassName('glyphicon glyphicon-refresh')[0]
+// Scoping this code as a function to prevent "already declared" errors
+function ss_refresh() {
+    // Create the event object
+    let eventObject = document.createEvent('Events');
+    eventObject.initEvent('click', true, false);
 
-// Dispatch the event
-refresh.dispatchEvent(eventObject)
+    // Grab the refresh button by class (only one element on the page uses this class)
+    let refresh = document.getElementsByClassName('glyphicon glyphicon-refresh')[0]
+
+    // Dispatch the event
+    refresh.dispatchEvent(eventObject)
+};
+
+ss_refresh();
