f5b4c4f8027c804327f18ad15803911be9eb2c3b
Matthew Becker <becker@tamu.edu>
2021-08-13 09:19:55 -0500

    Clean up options.js & options.html

--- src/options.js
+++ src/options.js
@@ -1,5 +1,4 @@
-// List of all shift types (may change from time to time, too many as is)
-const all_shifts = [
+const ALL_SHIFTS = [
     "Phones", 
     "Bomgar", 
     "Tier 2", 
@@ -13,104 +12,122 @@ const all_shifts = [
     "Project", 
     "Trainer"]
 
-// Range for before_minutes, 0-X
-let before_minutes_range = 5
+const MAXIMUM_ADVANCED_MINUTES = 5
 
-// Add a handler for the save button
-let save_config = document.getElementById("save_config");
-save_config.addEventListener("click", async () =>{
-    // Loop through the selected shift types
-    let selected_shifts = getSelectValuesFromFormSelect(document.getElementById("shifts"))
-    if (selected_shifts.length == 0){
-        window.alert("Please select at least one type of shift")
-        return;
-    } else {
-        let shifts_to_save = [];
-        selected_shifts.forEach(function(shift_idx) {
-            shifts_to_save.push(all_shifts[shift_idx])
-        });
-        // Save the configuration to storage.sync
-        chrome.storage.sync.set({shifts_to_show: shifts_to_save})
+function GetSelectedOptionsFromFormSelect(select) 
+{
+    var selectedOptions = [];
+    var options = select && select.options;
+
+    for (const option of options) {
+        if (option.selected) {
+            selectedOptions.push(option.value || option.text);
+        }
     }
+    return selectedOptions;
+}
 
-    // Update checkbox status
+function AddSaveButtonHandler(buttonID)
+{
+    let save_config_button = document.getElementById(buttonID);
+    save_config_button.addEventListener("click", SaveButtonHandler);
+}
+
+async function SaveButtonHandler()
+{
+    let selected_shifts = GetSelectedOptionsFromFormSelect(document.getElementById("shifts"))
+
+    if (selected_shifts.length == 0)
+    {
+        window.alert("Please select at least one type of shift");
+        return;
+    } 
+
+    SaveSelects(selected_shifts);
+    SaveCheckboxes();
+}
+
+async function SaveSelects(shifts)
+{
+    let shifts_to_save = [];
+    shifts.forEach(function(shift_idx) {
+        shifts_to_save.push(ALL_SHIFTS[shift_idx])
+    });
+
+    const advanced_minutes = GetSelectedOptionsFromFormSelect(
+        document.getElementById("before_minutes"))[0];
+
+    chrome.storage.sync.set({
+        shifts_to_show: shifts_to_save,
+        before_minutes: advanced_minutes
+    });
+}
+
+async function SaveCheckboxes()
+{
     chrome.storage.sync.set({
         send_empty_notification: document.getElementById("send_empty_notifications").checked,
-        // check_on_15: document.getElementById("check_on_15").checked,
         ss_remove_rows: document.getElementById("ss_remove_rows").checked,
-        ss_ignore_filter: document.getElementById("ss_ignore_filter").checked,
-
-        // Update before_minutes
-        before_minutes: getSelectValuesFromFormSelect(document.getElementById("before_minutes"))[0]
+        ss_ignore_filter: document.getElementById("ss_ignore_filter").checked
     });
-});
+}
 
-// [9] https://stackoverflow.com/questions/11821261/how-to-get-all-selected-values-from-select-multiple-multiple
-function getSelectValuesFromFormSelect(select) {
-    var result = [];
-    var options = select && select.options;
-    var opt;
-  
-    for (var i=0, iLen=options.length; i<iLen; i++) {
-      opt = options[i];
-  
-      if (opt.selected) {
-        result.push(opt.value || opt.text);
-      }
-    }
-    return result;
-  }
+function OnPageLoad()
+{
+    chrome.storage.sync.get({
+        schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All",
+        interval_minutes: 15,
+        range_minutes: 7,
+        send_empty_notification: false,
+        before_minutes: 0,
+        padding_minutes: 0,
+        shifts_to_show: ["Phones", "Bomgar", "Tier 2"],
+        ss_remove_rows: true,
+        ss_ignore_filter: false
+    }, 
+    function(configuration) 
+    {
+        FillFormSelects(configuration);
+        PreCheckCheckboxes(configuration)
+    });
+}
 
+function FillFormSelects(configuration) 
+{
+    FillShiftsSelect(configuration);
+    FillAdvancedMinutesSelect(configuration);
+}
 
-// Run when the page first loads to populate existing information
-function onLoad(){
-// Get the current configuration settings
-chrome.storage.sync.get({
-    schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All",
-    interval_minutes: 15,
-    range_minutes: 7,
-    send_empty_notification: false,
-    before_minutes: 0,
-    padding_minutes: 0,
-    shifts_to_show: ["Phones", "Bomgar", "Tier 2"],
-    ss_remove_rows: true,
-    ss_ignore_filter: false
-}, function(configuration_dict) {
-
-    // Populate the select with currently configured shift types
+function FillShiftsSelect(configuration) {
     let shifts = document.getElementById("shifts")
 
-    all_shifts.forEach(function(value, index){
-        // If the shift type is in shifts_to_shot, add it as a selected option, else just add it
-        if (configuration_dict.shifts_to_show.includes(value)){
+    ALL_SHIFTS.forEach(function(value, index){
+        if (configuration.shifts_to_show.includes(value)){
             shifts.innerHTML += `<option value=${index} selected>${value}</option>`
         } else {
             shifts.innerHTML += `<option value=${index}>${value}</option>`
         }
 
     });
-
-    // Pre-check checkboxes
-    if (configuration_dict.send_empty_notification){
-        document.getElementById("send_empty_notifications").checked = true;
-    }
-    if (configuration_dict.ss_remove_rows){
-        document.getElementById("ss_remove_rows").checked = true;
-    }
-    if (configuration_dict.ss_ignore_filter){
-        document.getElementById("ss_ignore_filter").checked = true;
-    }
-
-    // Populate the before_minutes select
-    let before_minutes = document.getElementById("before_minutes")
-    for (let i = 0; i <= before_minutes_range; i++){
-        if (i == configuration_dict.before_minutes){
-            before_minutes.innerHTML += `<option value=${i} selected>${i}</option>`
-        } else {
-            before_minutes.innerHTML += `<option value=${i}>${i}</option>`
-        }
-    }
-});
 }
 
-onLoad();
+function FillAdvancedMinutesSelect(configuration) {
+    let advanced_minutes = document.getElementById("before_minutes")
+    for (let i = 0; i <= MAXIMUM_ADVANCED_MINUTES; i++){
+        if (i == configuration.before_minutes){
+            advanced_minutes.innerHTML += `<option value=${i} selected>${i}</option>`
+        } else {
+            advanced_minutes.innerHTML += `<option value=${i}>${i}</option>`
+        }
+    }
+}
+
+function PreCheckCheckboxes(configuration)
+{
+    document.getElementById("send_empty_notifications").checked = configuration.send_empty_notification;
+    document.getElementById("ss_remove_rows").checked = configuration.ss_remove_rows;
+    document.getElementById("ss_ignore_filter").checked = configuration.ss_ignore_filter;
+}
+
+AddSaveButtonHandler("save_config")
+OnPageLoad();
