5e577adcc98f92b45cff94a28ac5087aacdf42c1
Matthew Becker <becker@tamu.edu>
2021-08-13 13:10:07 -0500

    Extract logic to functions

--- src/popup.js
+++ src/popup.js
@@ -1,37 +1,33 @@
-// Add a handler for the enable button
-let popup_enable = document.getElementById("popup_enable");
-popup_enable.addEventListener("click", async () =>{
-// Load configuration variables from storage (welcome to the highest level of callback hell (I didn't tab this because there are two many tabs as is))
-chrome.storage.sync.get({
-  schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All",  // This is here as a failsafe in the event the URL changes in the future and needs to be configured manually
-  interval_minutes : 15,
-  before_minutes: 0,  // Minutes before 00, 15, 30, 45 the alarm will trigger
-  padding_minutes: 5  // Upon activating the extension, number of minutes past 00, 15, 30, 45 where it will still trigger
-}, function(configuration_dict){
+function AddToggleButtonHandler(buttonID)
+{
+  const popup_enable = document.getElementById(buttonID);
+  popup_enable.addEventListener("click", ToggleButtonHandler);
+}
 
-  // Get the current status and swap it, activating and deacting the alarm as needed
-  chrome.storage.sync.get({status: false}, function(current_status){
-    // Technically I could just assign to !status.status, but this is safer if status = undefined
-    if (current_status.status == false){
-      // Verify the current tab has the correct URL
+function ToggleButtonHandler()
+{
+  chrome.storage.sync.get({
+    schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All", 
+    interval_minutes : 15,
+    before_minutes: 0,
+    padding_minutes: 5,
+    status: false,
+  }, function(configuration)
+  {
+    if (configuration.status){
       chrome.tabs.query({active: true, currentWindow: true}, function(data){
         console.log(data[0].url)
-        if (!data[0].url.endsWith(configuration_dict.schedulesource_url)){
+        if (!data[0].url.endsWith(configuration.schedulesource_url)){
           chrome.tabs.executeScript(undefined, {code: `window.alert("Please activate the extension after navigating to today's schedule");`})
           return;
         }
         
-      //* This code only runs if the URL comes back as correct ------------------------
-
-      // Set the status to true
       chrome.storage.sync.set({status: true}, function() {
-        console.log("Status: true")
-        update_status();
+        UpdateStatus();
       });
 
       // Store the tabID and start the timer in the callback to prevent race conditions
       chrome.storage.sync.set({tabId: data[0].id}, function() {
-        console.log(`tabID updated to ${data[0].id}`)
 
         // Store the WindowID (Async delay is fine, shouldn't cause race conditiions)
         chrome.windows.getCurrent({}, function(window){
@@ -40,73 +36,63 @@ chrome.storage.sync.get({
 
         // Calculate delayInMinutes (ex. time until minutes == 00, 15, 30, 45 - minutes_before)
         let now = new Date();
-        let alarmDelay_minutes = configuration_dict.interval_minutes - now.getMinutes() % configuration_dict.interval_minutes;
+        let alarmDelay_minutes = configuration.interval_minutes - now.getMinutes() % configuration.interval_minutes;
 
         // Modify alarmDelay_minutes based on configuration variables
         let run_now = false
-        if (now.getMinutes() % configuration_dict.interval_minutes < configuration_dict.padding_minutes){
+        if (now.getMinutes() % configuration.interval_minutes < configuration.padding_minutes){
           run_now = true;
         }
 
         // Check for a negative value after applying before_minutes
-        alarmDelay_minutes = alarmDelay_minutes - configuration_dict.before_minutes
+        alarmDelay_minutes = alarmDelay_minutes - configuration.before_minutes
         if (alarmDelay_minutes < 0) {
-          alarmDelay_minutes += configuration_dict.interval_minutes;
+          alarmDelay_minutes += configuration.interval_minutes;
           run_now = true
         }
 
-        // Only trigger on the minute
-        console.log(alarmDelay_minutes)
+        // Possibly causing issue #1
         let alarmStart_ms = (alarmDelay_minutes) * 60000 - (60 + now.getSeconds()) - (1000 + now.getMilliseconds());
-        console.log(alarmStart_ms)
+
         if (alarmStart_ms < 0){
-          alarmStart_ms += configuration_dict.interval_minutes * 60000;
+          alarmStart_ms += configuration.interval_minutes * 60000;
           run_now = true;
-          console.log("ERROR: You shouldn't be seeing this log")
         }
 
-        console.log(`Alarm starting in ${alarmStart_ms / 1000} seconds or ${alarmStart_ms / 60000} minutes`)
-        console.log(`Alarm also running now: ${run_now}`)
-
-        // Activate the alarm(s)
-        chrome.alarms.create('run',{
-          when: Date.now() + alarmStart_ms,
-          periodInMinutes: configuration_dict.interval_minutes
-        });
+        CreateRunAlarm(configuration.interval_minutes, alarmStart_ms);
 
         if (run_now){
-          chrome.alarms.create('run_once', {
-            when: Date.now() + 1000,
-          })
+          CreateRunOnceAlarm();
         }
       });
       })
 
     } else {
-      chrome.storage.sync.set({
-        status: false,
-        tabId: -1,
-        windowId: -1
-      }, function() {
-        console.log("Status: false")
-        update_status();
-
-        // Clear the alarm
-        chrome.alarms.clear('run', function() {
-          console.log("Alarm cleared")
-        })
-      });
+      ToggleOff();
     }
-  })
-});
-});
+  });
+}
 
+function ToggleOff(){
+  chrome.storage.sync.set({
+    status: false,
+    tabId: -1,
+    windowId: -1
+  }, 
+  function() 
+  {
+    UpdateStatus();
+    chrome.alarms.clear('run');
+  });
+}
 
-// Add an event handler for the run once button
-let popup_run_once = document.getElementById("popup_run_once");
-popup_run_once.addEventListener("click", async () =>{
-  console.log("Running once")
+function AddRunOnceButtonHandler(buttonID){
+  const run_once_button = document.getElementById("popup_run_once");
+  run_once_button.addEventListener("click", () => {RunOnceButtonHandler(run_once_button)});
+}
 
+function RunOnceButtonHandler(run_once_button)
+{
   // Get the schedulesource_url from storage
   chrome.storage.sync.get({schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All"}, function(configuration_dict){
 
@@ -117,35 +103,46 @@ popup_run_once.addEventListener("click", async () =>{
         return;
       }
 
-      // Store the WindowID (Async delay is fine, shouldn't cause race conditiions...)
       chrome.windows.getCurrent({}, function(window){
-        chrome.storage.sync.set({windowId: window.id})
+        chrome.storage.sync.set({
+          windowId: window.id,
+          tabId: data[0].id
+        },
+        CreateRunOnceAlarm)
       });
-
-      // Store the tabId and trigger the run_once alarm
-      chrome.storage.sync.set({tabId: data[0].id}, function(){
-        chrome.alarms.create('run_once',{
-          when: Date.now() + 1000,
-        });
-      })
     });
   });
 
-  // Change the text of the run_once button to "Running..."
-  popup_run_once.disabled = true;
-  popup_run_once.innerHTML = "<strong>Running...</strong>"
+  UpdateRunOnceText(run_once_button);
+}
 
-  // Change back after 11 seconds (run_once delay + background.js/refresh_delay)
+function CreateRunOnceAlarm()
+{
+  chrome.alarms.create('run_once',{
+    when: Date.now() + 1000,
+  });
+}
+
+function CreateRunAlarm(interval_minutes, alarmStart_ms)
+{
+  chrome.alarms.create('run',{
+    when: Date.now() + alarmStart_ms,
+    periodInMinutes: configuration.interval_minutes
+  });
+}
+
+function UpdateRunOnceText(run_once)
+{
+  run_once.disabled = true;
+  run_once.innerHTML = "<strong>Running...</strong>"
+
   setTimeout(() => {
-    popup_run_once.disabled = false;
-    popup_run_once.innerHTML = "<strong>Run once</strong>"
+    run_once.disabled = false;
+    run_once.innerHTML = "<strong>Run once</strong>"
   }, 6000);
+}
 
-});
-
-
-// Update the status text based on current operational status
-function update_status(){
+function UpdateStatus(){
   // Get the current application status and update
   chrome.storage.sync.get({status: false}, function(current_status){
     if (current_status.status == true){
@@ -156,23 +153,6 @@ function update_status(){
   });
 }
 
-
-update_status();  // Run update_status when the script is called
-
-
-//* TEMP CODE
-// let popup_test = document.getElementById("popup_test");
-// popup_test.addEventListener("click", async () =>{
-//   console.log("test button")
-
-//   // console.log("test alarm triggering in 1 second(s)")
-//   // chrome.alarms.create('run_once',{
-//   //   when: Date.now() + 1000,
-//   // })
-
-//   // chrome.storage.sync.get({tabId: -1}, function(dict){
-//   //   chrome.tabs.executeScript(dict.tabId, {file: scripts/color_schedule.js}, function(){
-//   //     console.log("Script injected")
-//   //   });
-//   // });
-// });
+AddToggleButtonHandler("popup_enable");
+AddRunOnceButtonHandler("popup_run_once");
+UpdateStatus();
