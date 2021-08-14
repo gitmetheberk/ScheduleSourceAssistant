20beb061d99d520847f0da1e13490d28601a04e9
Matthew Becker <becker@tamu.edu>
2021-08-14 10:58:55 -0500

    More functions, less comments
    Cleanup continues

--- src/popup.js
+++ src/popup.js
@@ -4,6 +4,47 @@ function AddToggleButtonHandler(buttonID)
   popup_enable.addEventListener("click", ToggleButtonHandler);
 }
 
+function CalculateRunAlarmDelay(configuration)
+{
+  const now = new Date();
+  let alarmDelay_minutes = configuration.interval_minutes - now.getMinutes() % configuration.interval_minutes;
+
+  let run_now = false
+  if (now.getMinutes() % configuration.interval_minutes < configuration.padding_minutes){
+    run_now = true;
+  }
+
+  alarmDelay_minutes = alarmDelay_minutes - configuration.before_minutes
+
+  if (alarmDelay_minutes < 0) {
+    alarmDelay_minutes += configuration.interval_minutes;
+    run_now = true
+  }
+
+  // Possibly causing issue #1
+  let alarmStart_ms = (alarmDelay_minutes) * 60000 - (60 + now.getSeconds()) - (1000 + now.getMilliseconds());
+
+  if (alarmStart_ms < 0){
+    alarmStart_ms += configuration.interval_minutes * 60000;
+    run_now = true;
+  }
+
+  return {
+    alarmStart_ms,
+    run_now
+  }
+}
+
+function VerifyURL(configuration, TabData)
+{
+  if (!TabData[0].url.endsWith(configuration.schedulesource_url)){
+    chrome.tabs.executeScript(undefined, {code: `window.alert("Please activate the extension after navigating to today's schedule");`})
+    return false;
+  }
+
+  return true;
+}
+
 function ToggleButtonHandler()
 {
   chrome.storage.sync.get({
@@ -14,66 +55,49 @@ function ToggleButtonHandler()
     status: false,
   }, function(configuration)
   {
-    if (configuration.status){
-      chrome.tabs.query({active: true, currentWindow: true}, function(data){
-        console.log(data[0].url)
-        if (!data[0].url.endsWith(configuration.schedulesource_url)){
-          chrome.tabs.executeScript(undefined, {code: `window.alert("Please activate the extension after navigating to today's schedule");`})
+    if (!configuration.status){
+      chrome.tabs.query({active: true, currentWindow: true}, function(data)
+      {
+        if (!VerifyURL(configuration, data))
+        {
           return;
         }
-        
-      chrome.storage.sync.set({status: true}, function() {
-        UpdateStatus();
-      });
 
-      // Store the tabID and start the timer in the callback to prevent race conditions
-      chrome.storage.sync.set({tabId: data[0].id}, function() {
+        chrome.windows.getCurrent({}, function(window)
+        {
+          chrome.storage.sync.set({
+            windowId: window.id,
+            tabId: data[0].id,
+            status: true
+          }, function() {
 
-        // Store the WindowID (Async delay is fine, shouldn't cause race conditiions)
-        chrome.windows.getCurrent({}, function(window){
-          chrome.storage.sync.set({windowId: window.id})
+            UpdateStatusOnPopup();
+
+            const 
+            {
+              alarmStart_ms,
+              run_now
+            } = CalculateRunAlarmDelay(configuration)
+
+            CreateRunAlarm(configuration.interval_minutes, alarmStart_ms);
+
+            if (run_now){
+              CreateRunOnceAlarm();
+            }            
+          });
         });
-
-        // Calculate delayInMinutes (ex. time until minutes == 00, 15, 30, 45 - minutes_before)
-        let now = new Date();
-        let alarmDelay_minutes = configuration.interval_minutes - now.getMinutes() % configuration.interval_minutes;
-
-        // Modify alarmDelay_minutes based on configuration variables
-        let run_now = false
-        if (now.getMinutes() % configuration.interval_minutes < configuration.padding_minutes){
-          run_now = true;
-        }
-
-        // Check for a negative value after applying before_minutes
-        alarmDelay_minutes = alarmDelay_minutes - configuration.before_minutes
-        if (alarmDelay_minutes < 0) {
-          alarmDelay_minutes += configuration.interval_minutes;
-          run_now = true
-        }
-
-        // Possibly causing issue #1
-        let alarmStart_ms = (alarmDelay_minutes) * 60000 - (60 + now.getSeconds()) - (1000 + now.getMilliseconds());
-
-        if (alarmStart_ms < 0){
-          alarmStart_ms += configuration.interval_minutes * 60000;
-          run_now = true;
-        }
-
-        CreateRunAlarm(configuration.interval_minutes, alarmStart_ms);
-
-        if (run_now){
-          CreateRunOnceAlarm();
-        }
-      });
       })
 
-    } else {
+    } 
+    else 
+    {
       ToggleOff();
     }
   });
 }
 
-function ToggleOff(){
+function ToggleOff()
+{
   chrome.storage.sync.set({
     status: false,
     tabId: -1,
@@ -81,7 +105,7 @@ function ToggleOff(){
   }, 
   function() 
   {
-    UpdateStatus();
+    UpdateStatusOnPopup();
     chrome.alarms.clear('run');
   });
 }
@@ -93,17 +117,17 @@ function AddRunOnceButtonHandler(buttonID){
 
 function RunOnceButtonHandler(run_once_button)
 {
-  // Get the schedulesource_url from storage
-  chrome.storage.sync.get({schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All"}, function(configuration_dict){
-
-    // Get the current tab and verify the url is correct
-    chrome.tabs.query({active: true, currentWindow: true}, function(data){
-      if (!data[0].url.endsWith(configuration_dict.schedulesource_url)){
-        chrome.tabs.executeScript(undefined, {code: `window.alert("Please activate the extension after navigating to today's schedule");`})
+  chrome.storage.sync.get({schedulesource_url: "schedulesource.net/Enterprise/TeamWork5/Emp/Sch/#All"}, function(configuration)
+  {
+    chrome.tabs.query({active: true, currentWindow: true}, function(data)
+    {
+      if (!VerifyURL(configuration, data))
+      {
         return;
       }
 
-      chrome.windows.getCurrent({}, function(window){
+      chrome.windows.getCurrent({}, function(window)
+      {
         chrome.storage.sync.set({
           windowId: window.id,
           tabId: data[0].id
@@ -127,7 +151,7 @@ function CreateRunAlarm(interval_minutes, alarmStart_ms)
 {
   chrome.alarms.create('run',{
     when: Date.now() + alarmStart_ms,
-    periodInMinutes: configuration.interval_minutes
+    periodInMinutes: interval_minutes
   });
 }
 
@@ -142,12 +166,16 @@ function UpdateRunOnceText(run_once)
   }, 6000);
 }
 
-function UpdateStatus(){
-  // Get the current application status and update
-  chrome.storage.sync.get({status: false}, function(current_status){
-    if (current_status.status == true){
+function UpdateStatusOnPopup()
+{
+  chrome.storage.sync.get({status: false}, function(current_status)
+  {
+    if (current_status.status)
+    {
       document.getElementById("status").innerHTML = "<p><strong>Status: Active</strong></p>";
-    } else {
+    } 
+    else 
+    {
       document.getElementById("status").innerHTML = "<p><strong>Status: Disabled</strong></p>";
     }
   });
@@ -155,4 +183,4 @@ function UpdateStatus(){
 
 AddToggleButtonHandler("popup_enable");
 AddRunOnceButtonHandler("popup_run_once");
-UpdateStatus();
+UpdateStatusOnPopup();
