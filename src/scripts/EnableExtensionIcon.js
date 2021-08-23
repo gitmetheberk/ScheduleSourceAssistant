3c7afec8371290c1a319d66152f6bdd83a81c6bc
Matthew Becker <becker@tamu.edu>
2021-08-22 20:23:25 -0500

    Remove comment in EnableExtensionIcon()

--- src/scripts/EnableExtensionIcon.js
+++ src/scripts/EnableExtensionIcon.js
@@ -1,6 +1,5 @@
 function EnableExtensionIcon()
 {
-    // [8] - Send a message to background.js to enable the icon to show popup.html
     chrome.runtime.sendMessage({"message": "activate_icon"});
 }
 
